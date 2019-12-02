0e2eed91caa560f6f02bf052b8abe9fcdd9ed239
soehina <kamuhina@gmail.com>
2019-12-02 17:48:28 +0900

    空投稿を防ぐ

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,6 +1,6 @@
 class Result < ApplicationRecord
-    # validates:number,{presence:true}
-    # validates:word,{presence:true,uniqueness: true}
+    validates:number,{presence:true}
+    validates:word,{presence:true,uniqueness: true}
     def self.search(search)
         search ? where('word LIKE ?', "%#{search}%") : all
     end
